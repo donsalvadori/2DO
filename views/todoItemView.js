c275d3dce249e7935526c3a99ee1804927961bc9
Chris Salvi <csalvi42@gmail.com>
2015-04-29 13:30:40 -0700

    added template

--- views/todoItemView.js
+++ views/todoItemView.js
@@ -22,7 +22,6 @@ var TodoItemView = Backbone.View.extend({
 	onClickToggle: function(){
 			this.model.toggle();
 			this.model.save();
-			console.log(this.model.toJSON());
 	},
 
 
@@ -33,8 +32,9 @@ var TodoItemView = Backbone.View.extend({
 
 		this.$el.toggleClass("completed", this.model.get("completed"));
 
-		var checked = this.model.get("completed") ? "checked" : "";
-		this.$el.html("<input id='toggle' type='checkbox'" + checked +"></input>" + this.model.escape("title") + "<button id='delete'>Delete</button>");
+		var template = $("#todoItemTemplate").html();
+		var html = Mustache.render(template, this.model.toJSON());
+		this.$el.html(html);	
 
 		return this;
 	}
